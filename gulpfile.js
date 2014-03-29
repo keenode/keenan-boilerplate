223dbe75489c9b2033082eae6758873ca87517ff
Keenan Staffieri <kstaffieri@gmail.com>
2014-03-28 21:27:49 -0700

    added bootstrap javascripts to gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,18 @@ var paths = {
 						],
 	scripts_compile:	[
 							'bower_components/jquery/dist/jquery.js',
-							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/affix.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/affix.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/alert.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/button.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/carousel.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/dropdown.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tab.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/transition.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/scrollspy.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/tooltip.js',
+							'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/popover.js',
 							'src/scripts/' + js_filename + '.js'
 						],
 	scripts_lint:		[
